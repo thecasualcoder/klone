843ef3d8bb267b25501eceee345ac361099cb445
Arunvel Sriram <arunvelsriram@gmail.com>
2018-11-02 11:10:11 +0530

    Pass resource name and type as argument

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
@@ -19,10 +20,15 @@ var toCluster string
 var namespace string
 
 var rootCmd = &cobra.Command{
-	Use:   "klone",
+	Use:   "klone <kind/name>",
 	Short: "Clone Kubernetes resources",
 	Long:  "Clone Kubernetes resources within a cluster or across different clusters",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		kindAndName := strings.Split(args[0], "/")
+		// kind := kindAndName[0]
+		name := kindAndName[1]
+
 		fromClusterConfig, err := kubeconfig.ConfigFor(fromCluster, kubeCfgFile)
 		if err != nil {
 			fmt.Println(err)
@@ -35,7 +41,7 @@ var rootCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		d, err := deployment.Get("utils", namespace, fromClusterConfig)
+		d, err := deployment.Get(name, namespace, fromClusterConfig)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -46,6 +52,8 @@ var rootCmd = &cobra.Command{
 			fmt.Println(err)
 			os.Exit(1)
 		}
+
+		fmt.Println("Deployment cloned successfully")
 	},
 }
 
